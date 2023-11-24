6dec1b414856b2fe61f18ad0579070bf0b67132d
Hameed Akshal <mailmeakshal@gmail.com>
2023-11-25 01:40:48 +0530

    ADDED main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.26.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+}
+
+module "EC2" {
+  source         = "./EC2"
+}
+
+module "PARAMETERS" {
+  source         = "./PARAMETERS"
+}
+
+#module "RDS" {
+#  source         = "./RDS"
+#}
+
+#module "EFS" {
+#  source         = "./EFS"
+#}
