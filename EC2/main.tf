cfd1fbf36d0511a8788a82c7e3da23ac4fe1ae3b
Hameed Akshal <mailmeakshal@gmail.com>
2023-11-25 04:17:10 +0530

    ADDED LAUNCH TEMPLATE AND ASG

--- EC2/main.tf
+++ EC2/main.tf
@@ -34,25 +34,65 @@ resource "aws_iam_instance_profile" "wordpress_instance_profile" {
   role = aws_iam_role.wordpress_role.name
 }
 
-resource "aws_instance" "web" {
-  ami                    = "ami-0230bd60aa48260c6"
-  instance_type          = "t2.micro"
+#resource "aws_instance" "web" {
+#  ami                    = "ami-0230bd60aa48260c6"
+#  instance_type          = "t2.micro"
 
  # key_name               = "akshal1.pem"
 
-  vpc_security_group_ids = ["sg-0babac8ae6c131880"]
-  subnet_id              = "subnet-0ccc8d427e5ab3c0f"
+#  vpc_security_group_ids = ["sg-0babac8ae6c131880"]
+#  subnet_id              = "subnet-0ccc8d427e5ab3c0f"
  # user_data = "${file("setup.sh")}"
 
-  associate_public_ip_address = true
+#  associate_public_ip_address = true
 
-  iam_instance_profile    = aws_iam_instance_profile.wordpress_instance_profile.name
+#  iam_instance_profile    = aws_iam_instance_profile.wordpress_instance_profile.name
+
+#  credit_specification {
+#    cpu_credits = "standard"
+#     }
+
+#  tags = {
+#    Name = "ak"                                            #instance_name
+#  }
+#}
+
+
+
+
+# Create Launch Template
+resource "aws_launch_template" "wordpress_launch_template" {
+  name          = "Wordpress"
+#  version_description = "Single server DB and App"
+
+  image_id      = "ami-0230bd60aa48260c6"  # Replace with the actual Amazon Linux 2023 AMI
+  instance_type = "t2.micro"  # or "t2.micro" or any other instance type
+  vpc_security_group_ids = ["sg-0babac8ae6c131880"]
+
+  
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.wordpress_instance_profile.name
+  }
 
   credit_specification {
     cpu_credits = "standard"
-     }
+  }
+  user_data = base64encode(file("./EC2/setup.sh"))
 
-  tags = {
-    Name = "ak"                                            #instance_name
+#  output "launch_template_id" {
+#  value = aws_launch_template.wordpress_launch_template.id
+#  }
+}
+
+# Create Auto Scaling Group
+resource "aws_autoscaling_group" "wordpress_asg" {
+  desired_capacity     = 1  # Adjust as needed
+  max_size             = 1  # Adjust as needed
+  min_size             = 1  # Adjust as needed
+  vpc_zone_identifier  = ["subnet-0ccc8d427e5ab3c0f"]  # Replace with your subnet ID
+  launch_template {
+    id      = aws_launch_template.wordpress_launch_template.id
+    version = "$Latest"
   }
 }
